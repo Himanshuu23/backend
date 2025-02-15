e26d97518f2186dfa484461128303e393265aaaa
Hi <sagarhimanshu472@gmail.com>
2025-02-15 23:13:29 +0530

    feat: removed address logic

--- controller/user.js
+++ controller/user.js
@@ -1,30 +1,27 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const getCoordinates = require("../utils/getCoordinates");
 
 const registerUser = async (req, res) => {
   console.log(req.body)
   try {
-    const { name, email, password, address } = req.body;
-    if (!name || !email || !password || !address) {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
       return res.status(400).json({ error: "Missing required fields" });
     }
 
-    const { lat, lon } = await getCoordinates(address);
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
       name,
       email,
       password: hashedPassword,
-      address,
-      location: { type: "Point", coordinates: [lon, lat] } 
     });
 
     await user.save();
     res.status(201).json({ message: "User registered successfully", user });
   } catch (error) {
+    console.log('Error at register :',error)
     res.status(400).json({ error: error.message });
   }
 };
@@ -39,7 +36,7 @@ const loginUser = async (req, res) => {
     }
 
     // process.env.JWT_SECRET
-    const token = jwt.sign({ userId: user._id }, 'secretkey', { expiresIn: "1h" });
+    const token = jwt.sign({ userId: user._id }, 'process.env.JWT_SECRET', { expiresIn: "1h" });
     res.json({ token });
   } catch (error) {
     console.log(error)
